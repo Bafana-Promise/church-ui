1c11c8a143b474eb8a5c82f6dc97d0e8b2503f0e
Droshka <bafanapromise1@gmail.com>
2021-12-23 11:40:34 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LogRegComponent } from './log-reg/log-reg.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogRegComponent
   ],
   imports: [
     BrowserModule,
